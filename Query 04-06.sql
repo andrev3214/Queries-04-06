6de2fc41e64d708bc3b217531c1badb10f03dfaa
andrev3214 <avbermudez@uamv.edu.ni>
2026-06-04 07:48:22 -0600

    Update Query 04-06.sql

--- Query 04-06.sql
+++ Query 04-06.sql
@@ -1,5 +1,16 @@
+use master;
+go
+
+if(exists(select * from sys.databases where name = 'HospitalDB'))
+begin
+	drop database HospitalDB
+end
+go
+
 create database HospitalDB
 go
 
+
 use HospitalDB
 go
+
